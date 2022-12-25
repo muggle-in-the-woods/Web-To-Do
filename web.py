30c6997142fcd43e7a3a6980cfb0e9c1f8e2f472
Taras <tarvar988@gmail.com>
2022-12-25 10:42:20 +0200

    Initial commit

---
+++ web.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    print(todo)
+    todos.append(todo)
+    functions.write_todos(todos)
+
+st.title('My To-do App')
+st.subheader('This is a subheader')
+st.write('This app was made by student')
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label='Enter a to-do', placeholder='Add new to-do...', on_change=add_todo, key='new_todo')
